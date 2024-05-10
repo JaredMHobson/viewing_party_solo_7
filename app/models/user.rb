c1e8f1f5a1f17bd9e3ad3b46fc3bdb3cad4f97f3
Jared Hobson <jaredmhobson@gmail.com>
2024-05-09 23:38:58 -0700

    Complete US7 tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-   validates_presence_of :name, :email
-   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_presence_of :name, :email
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-   has_many :user_parties
-   has_many :viewing_parties, through: :user_parties
+  has_many :user_parties
+  has_many :viewing_parties, through: :user_parties
+
+  def hosted_parties
+    viewing_parties.where('user_parties.host = true')
+  end
+
+  def guest_parties
+    viewing_parties.where('user_parties.host = false')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,22 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#  movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#  Character.create(name: "Luke", movie: movies.first)
 
 # create Users
-10.times do 
-   User.create!(name: Faker::Name.name, email: Faker::Internet.email)
+10.times do
+  User.create!(name: Faker::Name.name, email: Faker::Internet.email)
 end
 
 # create Parties
-5.times do 
-   ViewingParty.create!(duration: rand(0..240), date: Faker::Date.forward(days: rand(1..14)), start_time: Time.new.strftime("%H:%M"))
-end
+ViewingParty.create!(duration: rand(180..240), date: Faker::Date.forward(days: rand(1..14)), start_time: Time.new.strftime("%H:%M"), movie_id: 767)
+ViewingParty.create!(duration: rand(180..240), date: Faker::Date.forward(days: rand(1..14)), start_time: Time.new.strftime("%H:%M"), movie_id: 330459)
+ViewingParty.create!(duration: rand(180..240), date: Faker::Date.forward(days: rand(1..14)), start_time: Time.new.strftime("%H:%M"), movie_id: 7446)
+ViewingParty.create!(duration: rand(180..240), date: Faker::Date.forward(days: rand(1..14)), start_time: Time.new.strftime("%H:%M"), movie_id: 157336)
+ViewingParty.create!(duration: rand(180..240), date: Faker::Date.forward(days: rand(1..14)), start_time: Time.new.strftime("%H:%M"), movie_id: 11817)
 
-# set Hosts 
+# set Hosts
 UserParty.create!(viewing_party: ViewingParty.first, user: User.first, host: true)
 UserParty.create!(viewing_party: ViewingParty.second, user: User.second, host: true)
 UserParty.create!(viewing_party: ViewingParty.third, user: User.third, host: true)
@@ -27,7 +29,7 @@ UserParty.create!(viewing_party: ViewingParty.last, user: User.fifth, host: true
 UserParty.create!(viewing_party: ViewingParty.first, user: User.second, host: false)
 UserParty.create!(viewing_party: ViewingParty.first, user: User.third, host: false)
 UserParty.create!(viewing_party: ViewingParty.second, user: User.fourth, host: false)
-UserParty.create!(viewing_party: ViewingParty.second, user: User.fourth, host: false)
+UserParty.create!(viewing_party: ViewingParty.second, user: User.first, host: false)
 UserParty.create!(viewing_party: ViewingParty.last, user: User.second, host: false)
 UserParty.create!(viewing_party: ViewingParty.last, user: User.first, host: false)
-UserParty.create!(viewing_party: ViewingParty.last, user: User.last, host: false)
+UserParty.create!(viewing_party: ViewingParty.last, user: User.third, host: false)

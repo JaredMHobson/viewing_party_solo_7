a11de2a4f7858a589c21630494c3b27cb80edbbf
Jared Hobson <jaredmhobson@gmail.com>
2024-05-21 15:28:57 -0700

    Move login functionality to sessions controller

--- spec/features/sessions/login_spec.rb
+++ spec/features/sessions/login_spec.rb
@@ -42,4 +42,27 @@ RSpec.describe "Logging In" do
 
     expect(page).to have_content("Invalid email address or password.")
   end
+
+  it 'can log out once you are logged in' do
+    user = User.create(name: "Tarzan", email: 'tarzan113@email.com', password: "hunter8")
+
+    visit root_path
+
+    expect(page).to_not have_button('Log Out')
+
+    click_on "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    within '.login_form' do
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+
+      click_on "Log In"
+    end
+
+    click_on 'Log Out'
+
+    expect(page).to have_content('Logged out successfully.')
+  end
 end

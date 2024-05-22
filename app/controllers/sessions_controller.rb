2f92ae1093a4605917461bc4ed5871e8126e29fd
Jared Hobson <jaredmhobson@gmail.com>
2024-05-21 20:08:53 -0700

    Add location cookie when logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 
     if user.authenticate(params[:password])
       session[:user_id] = user.id
+      cookies[:location] = params[:location]
       flash[:success] = "Welcome, #{user.name}"
       redirect_to user_path(user)
     else
--- spec/features/sessions/login_spec.rb
+++ spec/features/sessions/login_spec.rb
@@ -65,4 +65,54 @@ RSpec.describe "Logging In" do
 
     expect(page).to have_content('Logged out successfully.')
   end
+
+  it 'has a field to enter your location that is stored within a cookie and your location will be visible on the landing page' do
+    user = User.create(name: 'Tarzan', email: 'tarzan113@email.com', password: 'hunter8')
+
+    visit root_path
+
+    click_on 'Log In'
+
+    expect(current_path).to eq(login_path)
+
+    within '.login_form' do
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      fill_in :location, with: 'the jungle'
+
+      click_on "Log In"
+    end
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_content('Location: the jungle')
+  end
+
+  it 'when you log out and click log in again, the location field will already be filled out with your previously entered location' do
+    user = User.create(name: 'Tarzan', email: 'tarzan113@email.com', password: 'hunter8')
+
+    visit root_path
+
+    click_on 'Log In'
+
+    expect(current_path).to eq(login_path)
+
+    within '.login_form' do
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      fill_in :location, with: 'the jungle'
+
+      click_on "Log In"
+    end
+
+    expect(page).to have_content('Location: the jungle')
+
+    click_on 'Log Out'
+
+    click_on 'Log In'
+
+    within '.login_form' do
+      expect(page).to have_field(:location, with: 'the jungle')
+    end
+  end
 end

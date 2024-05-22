3f5868afafca64f3e6967078f4d3a55bedd15a39
Jared Hobson <jaredmhobson@gmail.com>
2024-05-21 21:22:26 -0700

    Add conditional to show list of userson root path and remove links to their show pages

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -18,13 +18,30 @@ RSpec.describe 'Root Page, Welcome Index', type: :feature do
       expect(page).to have_selector(:link_or_button, 'Create New User')
     end
 
-    it "They see a list of existing users, which links to the individual user's dashboard" do
+    it 'when logged in, they see a list of existing users email addresses' do
+      click_on('Log In')
+
+      within '.login_form' do
+        fill_in :email, with: @user_1.email
+        fill_in :password, with: @user_1.password
+
+        click_on "Log In"
+      end
+
+      visit root_path
+
       within('#existing_users') do
         expect(page).to have_content(User.first.email)
         expect(page).to have_content(User.last.email)
-        expect(page).to have_link("#{User.first.email}", href: "users/#{User.first.id}")
-        expect(page).to have_link("#{User.last.email}", href: "users/#{User.last.id}")
       end
+    end
+
+    it 'when not logged in, they do not see the list of existing users and only see the create new user button' do
+      expect(page).to_not have_css('#existing_users')
+      expect(page).to_not have_content(User.first.email)
+      expect(page).to_not have_content(User.last.email)
+
+      expect(page).to have_selector(:link_or_button, 'Create New User')
     end
 
     it 'They see a link to go back to the landing page (present at the top of all pages)' do
